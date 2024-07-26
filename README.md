34e3c3e54bd2b60d621881480578f60ebd2bd0e0
zzuse <zzuse@outlook.com>
2024-07-26 15:38:02 -0500

    led light 2

--- README.md
+++ README.md
@@ -18,4 +18,5 @@ Tools -> Board
 
 # Chapters
 *   Blinking LED on main board -- commit 6f37518a857d7f42e69f3f8938e0c2310668ead9
-*   Blinking LED on bread board ![Layout](/images/1.jpg) -- commit 
+*   Blinking LED on bread board  -- commit f35f20f3d96d4bc8933ee1c3e9ad5b2824811e97 ![Layout](/images/1.jpg)
+*   Blinking LED on bread board  -- commit ![Layout](/images/2.jpg)
--- sketch_jul25a/sketch_jul25a.ino
+++ sketch_jul25a/sketch_jul25a.ino
@@ -1,12 +1,53 @@
+int pin2 = 2;
+int pin3 = 3;
+int pin4 = 4;
+int pin5 = 5;
+int pin9 = 9;
+int waitTime = 1000;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(8, OUTPUT);
+  pinMode(pin2, OUTPUT);
+  pinMode(pin3, OUTPUT);
+  pinMode(pin4, OUTPUT);
+  pinMode(pin5, OUTPUT);
+  pinMode(pin9, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(500);
-  digitalWrite(8, HIGH);
-  delay(500);
-  digitalWrite(8, LOW);
+  digitalWrite(pin2, LOW);
+  digitalWrite(pin3, LOW);
+  digitalWrite(pin4, LOW);
+  digitalWrite(pin5, LOW);
+  digitalWrite(pin9, HIGH);
+  delay(waitTime);
+
+  digitalWrite(pin2, HIGH);
+  digitalWrite(pin3, LOW);
+  digitalWrite(pin4, LOW);
+  digitalWrite(pin5, LOW);
+  digitalWrite(pin9, LOW);
+  delay(waitTime);
+
+  digitalWrite(pin2, LOW);
+  digitalWrite(pin3, HIGH);
+  digitalWrite(pin4, LOW);
+  digitalWrite(pin5, LOW);
+  digitalWrite(pin9, LOW);
+  delay(waitTime);
+
+  digitalWrite(pin2, LOW);
+  digitalWrite(pin3, LOW);
+  digitalWrite(pin4, HIGH);
+  digitalWrite(pin5, LOW);
+  digitalWrite(pin9, LOW);
+  delay(waitTime);
+
+  digitalWrite(pin2, LOW);
+  digitalWrite(pin3, LOW);
+  digitalWrite(pin4, LOW);
+  digitalWrite(pin5, HIGH);
+  digitalWrite(pin9, LOW);
+  delay(waitTime);
 }
