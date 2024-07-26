e1168981e2f5bc5031b8c346578182277dbda93c
zzuse <zzuse@outlook.com>
2024-07-26 18:00:35 -0500

    analog read and write

--- sketch_jul26a/README.md
+++ sketch_jul26a/README.md
@@ -1,3 +1,4 @@
 Chapters
 
-* LED Analog Write -- commit
+* LED Analog Write -- commit b57137bc63197d785cf26a48e3bcf62e740b050c
+* LED Analog Read -- commit ![ref](/images/3.jpg)
--- sketch_jul26a/sketch_jul26a.ino
+++ sketch_jul26a/sketch_jul26a.ino
@@ -1,16 +1,33 @@
+// PIN ~3 ~5 ~6 ~9 ~10 ~11 have analog out
 int greenPin = 9;
+int readPin = A3;
+int V2 = 0;
+float V3 = 0;
 int bright = 127;
+int delayTime = 500;
+
 void setup()
 {
     // put your setup code here, to run once:
     pinMode(greenPin, OUTPUT);
+    pinMode(readPin, INPUT);
+    Serial.begin(9600);
 }
 
 void loop()
 {
     // put your main code here, to run repeatedly:
-    // 255 means most time is 5v
-    // 5 means most time is 0v
-    // 127 means half time 5v, half time 0v
+    // Write 255 means most time is 5v
+    // Write 0 means most time is 0v
+    // Write 127 means half time 5v, half time 0v
     analogWrite(greenPin, bright);
+    // Read from 0~1023
+    // 0V ~ 0
+    // 5V ~ 1023
+    // Read after Write 127(half time 5v, half time 0v) ~ ...0,0,0,967,966,667...
+    V2 = analogRead(readPin);
+    Serial.println(V2);
+    V3 = (5./1023.) * V2;
+    Serial.println(V3);
+    delay(delayTime);
 }
