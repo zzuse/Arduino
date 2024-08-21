aa29ca040fd87afd73902c65e379327f56f06ea2
zzuse <zzuse@outlook.com>
2024-08-21 01:24:14 -0500

    LCD with DHT11

---
+++ sketch_aug20a/README.md
@@ -0,0 +1,3 @@
+Chapters
+* DHT11 humidity and temperature -- commit 8ef7bf877c0bd49459f9d31104d8d337b07e7256 ![ref](/images/20.jpg)
+* LCD display with DHT11 humidity and temperature -- commit 8ef7bf877c0bd49459f9d31104d8d337b07e7256 ![ref](/images/21.jpg)
--- sketch_aug20a/sketch_aug20a.ino
+++ sketch_aug20a/sketch_aug20a.ino
@@ -1,23 +1,40 @@
 #include "DHT11.h"
+#include <LiquidCrystal.h>
+int rs = 7;
+int en = 8;
+int d4 = 9;
+int d5 = 10;
+int d6 = 11;
+int d7 = 12;
 
-int sensePin =2;
+LiquidCrystal lcd(rs,en,d4,d5, d6, d7);
+
+int sensePin = 2;
+// using DHT11 V2.1.0 library
 DHT11 HT(sensePin);
 int humidity;
 int tempC;
 int setTime = 500;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  delay(setTime);
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    lcd.begin(16,2);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  HT.readTemperatureHumidity(tempC, humidity);
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.println(" %");
-  Serial.println(tempC);
-  delay(setTime);
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    HT.readTemperatureHumidity(tempC, humidity);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Humidity: ");
+    lcd.print(humidity);
+    lcd.println("%   ");
+    lcd.setCursor(0, 1);
+    lcd.print("Temperature: ");
+    lcd.print(tempC);
+    lcd.println("C");
+    delay(setTime);
 }
